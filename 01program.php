782a88cf218d2bf7dfa758e46a5f0a93b50709aa
maciejf12 <50989125+maciejf12@users.noreply.github.com>
2019-05-24 13:37:22 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -3,5 +3,6 @@ echo("Witaj");
 echo("<br> Jestem programem 01");
 echo("<br> Test 11111");
 echo("<br> Test 22222");
-echo("<br> Dalsza część programu")
+echo("<br> Dalsza część programu");
+echo("<br> Dalsza część programu 22222 z Githuba");
 ?>
